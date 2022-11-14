ba5094716af2ae844b885d24a4f5949834dc7996
Demetrius Braga <demetrius.braga@gmail.com>
2022-11-14 10:15:09 -0300

    Project created

---
+++ backend/src/test/java/com/dbraga/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbraga.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
