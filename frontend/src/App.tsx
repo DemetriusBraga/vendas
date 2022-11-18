35c2eee28813f70bd630932fe53f0567095d5788
Demetrius Braga <demetrius.braga@gmail.com>
2022-11-18 10:40:46 -0300

    Send notification and toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Header from "./components/Hearder";
-import NotificationButton from "./components/NotificationButton";
 import SalesCard from "./components/SalesCard";
 
 function App() {
   return (
     <div>
+      <ToastContainer />
       <Header />
       <main>
         <section id="sales">
